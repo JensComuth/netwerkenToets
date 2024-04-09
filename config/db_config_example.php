162f156107f1a6fd37e420ef15150582cd570441
Jens Comuth <131358917+JensComuth@users.noreply.github.com>
2024-04-09 09:12:35 +0200

    commit 1

--- config/db_config_example.php
+++ config/db_config_example.php
@@ -1,9 +1,9 @@
 <?php
 
 $dbhost = 'localhost:3306';
-$dbuser = 'netwerken_user_2024';
-$dbpass = 'netwerken2024!';
-$dbname = 'netwerken_2024';
+$dbuser = 'net24jcomuthToets';
+$dbpass = 'Ch!llSk!l#!1';
+$dbname = 'net24jcomuth_netwerkenToetsDB';
 
 $conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 if($conn->connect_error)
